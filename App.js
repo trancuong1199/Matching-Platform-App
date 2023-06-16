54f4577a4d00c50414e21a89bf5be860e84d4c86
phamHoc <phamhoc2902@gmail.com>
2023-06-16 14:55:57 +0700

    initial App and install library react-native-base edit readme

---
+++ App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import React from 'react';
+import {NativeBaseProvider, Text, Box} from 'native-base';
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+        <Text>Open up App.js to start working on your app!</Text>
+      </Box>
+    </NativeBaseProvider>
+  );
+}
