48210d54eef804cd6477174731bf9b6cd91ceb1d
triofnguyen114 <nguyenminhtri11042000@gmail.com>
2023-06-17 14:03:08 +0700

    Style bottomtab

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {NativeBaseProvider, Text, Box} from 'native-base';
-export default function App() {
+import {NavigationContainer} from '@react-navigation/native';
+import BottomTabs from './src/navigations/BottomTabs';
+
+const App = () => {
   return (
-    <NativeBaseProvider>
-      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-        <Text>Open up App.js to start working on your app!</Text>
-      </Box>
-    </NativeBaseProvider>
+    <NavigationContainer>
+      <BottomTabs />
+    </NavigationContainer>
   );
-}
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
---
+++ src/screen/network/NetworkScreen.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const NetworkScreen = () => {
+  return (
+    <View>
+      <Text>NetworkScreen</Text>
+    </View>
+  );
+};
+
+export default NetworkScreen;
+
+const styles = StyleSheet.create({});
