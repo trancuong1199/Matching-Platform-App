944f77eee0b9a79a6dd009f0a1c2951e1a3d9b6d
anhChillLe <chieu280200@gmail.com>
2023-06-19 13:25:29 +0700

    no message

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import BottomTabs from './src/navigations/BottomTabs';
 
 const App = () => {
   return (
     <NavigationContainer>
-    <BottomTabs />
-  </NavigationContainer>
+      <BottomTabs />
+    </NavigationContainer>
   );
 }
 
