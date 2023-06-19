7d0a9939d327c92387c76169cab54a489bccc984
anhChillLe <chieu280200@gmail.com>
2023-06-19 11:27:39 +0700

    OK

--- App.js
+++ App.js
@@ -6,11 +6,13 @@
  */
 import React from 'react';
 import {NativeBaseProvider, Text, Box} from 'native-base';
+import FilterScreen from './src/screen/filter/filter';
 export default function App() {
   return (
     <NativeBaseProvider>
       <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-        <Text>Open up App.js to start working on your app!</Text>
+        {/* <Text>Open up App.js to start working on your app!</Text> */}
+        <FilterScreen/>
       </Box>
     </NativeBaseProvider>
   );
