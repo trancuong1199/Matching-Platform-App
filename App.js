5d1987893725d17a68efbf163696e4f78fd4b2c6
anhChillLe <chieu280200@gmail.com>
2023-06-19 11:29:10 +0700

    OK

--- App.js
+++ App.js
@@ -11,8 +11,7 @@ export default function App() {
   return (
     <NativeBaseProvider>
       <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-        {/* <Text>Open up App.js to start working on your app!</Text> */}
-        <FilterScreen/>
+        <Text>Open up App.js to start working on your app!</Text>
       </Box>
     </NativeBaseProvider>
   );
